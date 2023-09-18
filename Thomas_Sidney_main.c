1662e42a06b37f87296044480574e2be17006983
Sidney <sid.14boys@yahoo.fr>
2023-09-17 18:11:05 -0700

    minor updt (Makefile) + creation .c file

---
+++ Thomas_Sidney_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argumentCount, char *argumentValues[])
+{
+    // * READ USER INPUT
+
+    // * PARSE USER INPUT
+
+    // * EXECUTE COMMANDS
+}
