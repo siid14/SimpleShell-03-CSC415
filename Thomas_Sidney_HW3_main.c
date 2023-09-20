521ccfd9423279fa30454dcae2f3e82e66a6e689
Sidney <sid.14boys@yahoo.fr>
2023-09-19 19:20:46 -0700

    Restructuring code

--- Thomas_Sidney_HW3_main.c
+++ Thomas_Sidney_HW3_main.c
@@ -11,77 +11,84 @@ int main(int argumentCount, char *argumentValues[])
     // * READ USER INPUT
     char input[187]; // store user input command of 1024 characters
 
-    // while (1) // infinite loop
-    // {
-    printf("YourShell> ");              // indicate shell ready to accept user input
-    fgets(input, sizeof(input), stdin); // read the user input + store in input
-
-    // handle input error
-    if (fgets(input, sizeof(input), stdin) == NULL)
+    while (1) // infinite loop
     {
-        // check if fgets encountered EOF
-        if (feof(stdin))
+        printf("YourShell> ");              // indicate shell ready to accept user input
+        fgets(input, sizeof(input), stdin); // read the user input + store in input
+
+        // handle input error
+        if (fgets(input, sizeof(input), stdin) == NULL)
         {
-            // gracefully exit on EOF without reporting an error
-            printf("Exiting the shell.\n");
-            // break;
+            // check if fgets encountered EOF
+            if (feof(stdin))
+            {
+                // gracefully exit on EOF without reporting an error
+                printf("Exiting the shell.\n");
+                return 0; // ? break to use instead
+            }
+            else
+            {
+                // handle and report other input errors
+                perror("fgets");
+                exit(1); // exit the shell on error
+            }
         }
-        else
+        if (strlen(input) == 1)
         {
-            // handle and report other input errors
-            perror("fgets");
-            exit(1); // exit the shell on error
+            printf("Empty string entered, please enter a valid string\n");
+            perror("Empty string error");
+            continue;
         }
-    }
-    //}
-    printf("Input size (including newline): %zu\n", strlen(input));
-    printf("Input size (excluding newline): %zu\n", strlen(input) - 1);
 
-    // remove trailing newline character if present
-    if (input[strlen(input) - 1] == '\n') // check if last char in input is a newline character
-    {
-        input[strlen(input) - 1] = '\0'; // replace '\n' with null terminator '\0'
-    }
-    // }
+        printf("Input size (including newline): %zu\n", strlen(input));
+        printf("Input size (excluding newline): %zu\n", strlen(input) - 1);
 
-    // * PARSE USER INPUT
-    char *command = strtok(input, " "); // tokenize first string of user input
-    printf("Command: %s\n", command);
+        // remove trailing newline character if present
+        if (input[strlen(input) - 1] == '\n') // check if last char in input is a newline character
+        {
+            input[strlen(input) - 1] = '\0'; // replace '\n' with null terminator '\0'
+        }
+        // }
 
-    char *args[32];
-    // ** store remaining input string (arguments) into args
-    int arg_count = 0;
+        // * PARSE USER INPUT
+        char *command = strtok(input, " "); // tokenize first string of user input
+        printf("Command: %s\n", command);
 
-    while ((args[arg_count] = strtok(NULL, " "))) // tokenize each remaining string of input
-                                                  // until no more found
-    {
-        arg_count++;
-    }
-    printf("Number of argument in args: %d\n", arg_count);
+        char *args[32];
+        // ** store remaining input string (arguments) into args
+        int arg_count = 0;
 
-    // * EXECUTE COMMANDS
-    pid_t child_pid = fork(); // create a new process (child).
+        while ((args[arg_count] = strtok(NULL, " "))) // tokenize each remaining string of input
+                                                      // until no more found
+        {
+            arg_count++;
+        }
+        printf("Number of argument in args: %d\n", arg_count);
 
-    if (child_pid == 0) // fork succeed to create a child process
-    {
-        // child process
-        execvp(command, args); // execute the specified command with given arguments
+        // * EXECUTE COMMANDS
+        pid_t child_pid = fork(); // create a new process (child).
 
-        // if execvp() fails, handle the error
-        perror("execvp"); // print an error message
-        exit(1);          // exit the child process with a non-zero status to indicate an error
-    }
-    else if (child_pid < 0) // fork failed to create a child process
-    {
-        perror("fork"); // print an error message
-    }
-    else // parent process
-    {
-        int status;                     // variable to store the child process's exit status
-        waitpid(child_pid, &status, 0); // wait for the child process to complete
+        if (child_pid == 0) // fork succeed to create a child process
+        {
+            // child process
+            execvp(command, args); // execute the specified command with given arguments
 
-        // print the child PID and return result (exit status)
-        printf("Child PID: %d\n", child_pid);
-        printf("Return Result: %d\n", status);
+            // if execvp() fails, handle the error
+            perror("execvp"); // print an error message
+            exit(1);          // exit the child process with a non-zero status to indicate an error
+        }
+        else if (child_pid < 0) // fork failed to create a child process
+        {
+            perror("fork"); // print an error message
+        }
+        else // parent process
+        {
+            int status;                     // variable to store the child process's exit status
+            waitpid(child_pid, &status, 0); // wait for the child process to complete
+
+            // print the child PID and return result (exit status)
+            printf("Child PID: %d\n", child_pid);
+            printf("Return Result: %d\n", status);
+        }
     }
 }
